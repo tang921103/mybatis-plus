01b595cf65f36f6f68a15a3e75a100a6d540e32d
tangqiu <tangqiuemail@sina.cn>
2019-02-20 22:46:53 +0800

    'end'

--- core/src/main/java/com/zsm/core/mapper/FuncMapper.java
+++ core/src/main/java/com/zsm/core/mapper/FuncMapper.java
@@ -1,7 +1,11 @@
 package com.zsm.core.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zsm.core.entity.FuncEntity;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +16,6 @@ import com.zsm.core.entity.FuncEntity;
  * @since 2019-02-20
  */
 public interface FuncMapper extends BaseMapper<FuncEntity> {
-
+    @Select("select * from dc_func where id <10")
+    List<FuncEntity> getAll(QueryWrapper<FuncEntity> entityQueryWrapper);
 }
--- core/src/test/java/com/zsm/core/CoreApplicationTests.java
+++ core/src/test/java/com/zsm/core/CoreApplicationTests.java
@@ -29,6 +29,8 @@ public class CoreApplicationTests {
     private UserMapper userMapper;
     @Resource
     private FuncService funcServiceImpl;
+    @Resource
+    private FuncMapper funcMapper;
     @Test
     public void contextLoads() {
     }
@@ -95,5 +97,45 @@ public class CoreApplicationTests {
         Map<String,Object> map1 = funcServiceImpl.getMap(queryWrapper);
         System.out.println(map1);
 //        System.out.println(entity);
+    }
+
+    /**
+     * wrapper构造器
+     */
+    @Test
+    public void wrapperTest(){
+        QueryWrapper<FuncEntity> queryWrapper = new QueryWrapper<>();
+        Map<String,Object> map = new HashMap<String,Object>();
+//        map.put("id",1);
+//        map.put("dc_func_pid",null);
+//        queryWrapper.allEq(map);
+//        queryWrapper.eq("dc_func_name","数据管理");
+//        queryWrapper.ne("dc_func_name","数据管理");
+//        queryWrapper.lt("id",10);//less than 小于
+//        queryWrapper.gt("id",100);
+//        queryWrapper.le("id",30);
+//        queryWrapper.ge("id",20);
+//        queryWrapper.between("id",1,11);
+//        queryWrapper.notBetween("id",10,100);
+//        queryWrapper.like("dc_func_name","数据");
+//        queryWrapper.notLike("dc_func_name","管理");
+//        queryWrapper.likeLeft("dc_func_name","管理");
+//        queryWrapper.likeRight("dc_func_name","数据");
+//        queryWrapper.isNull("dc_func_pid");
+//        queryWrapper.isNotNull("dc_func_pid");
+        queryWrapper.or(id->id.lt("id",10).gt("id",5));
+        List<FuncEntity> list  = funcServiceImpl.list(queryWrapper);
+        for(FuncEntity e : list){
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * 自定义sql
+     */
+    @Test
+    public void sql(){
+        List<FuncEntity> list = funcMapper.getAll(new QueryWrapper<FuncEntity>());
+        System.out.println(list);
     }
 }
